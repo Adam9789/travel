35c7da95286b43f3c582aab8e548e45279ff66db
AdamLiu <1413038922@qq.com>
2019-02-13 19:50:30 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    hello
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
-<style>
+<style lang="">
 </style>
