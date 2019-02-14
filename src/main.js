96951215bbc4d2c911f0f6ad6acc27126adffa9f
AdamLiu <1413038922@qq.com>
2019-02-14 11:24:57 +0800

    add homeheader

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import './assets/styles/border.css'
-import './assets/styles/resets.css'
+import 'styles/border.css'
+import 'styles/resets.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <div class="iconfont backicon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+        城市
+        <span class="iconfont arrowicon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    line-height 0.86rem
+    display flex
+    background $bgColor
+    color #fff
+    .header-left
+        width .64rem
+        float left
+        .backicon
+            font-size .4rem
+            text-align center
+    .header-input
+        flex 1
+        border-radius .1rem
+        background #ffffff
+        margin-top .12rem
+        margin-left .2rem
+        padding-left .2rem
+        height .62rem
+        line-height .62rem
+        color #ccc
+    .header-right
+        float right
+        width 1.24rem
+        text-align center
+        .arrowicon
+            font-size .24rem
+
+</style>
