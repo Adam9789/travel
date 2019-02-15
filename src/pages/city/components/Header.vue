c929f881d045a4b329cb9370803cb4731b4c3728
AdamLiu <1413038922@qq.com>
2019-02-15 10:34:24 +0800

    cityheader

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <div class="iconfont backicon">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  overflow hidden
+  height .86rem
+  line-height .86rem
+  text-align center
+  color #fff
+  background $bgColor
+  font-size .36rem
+  position relative
+  .backicon
+    width .64rem
+    text-align center
+    font-size .4rem
+    position absolute
+    top 0
+    left 0
+    color #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题
     </div>
+    <router-link to="/city">
     <div class="header-right">
         {{this.city}}
         <span class="iconfont arrowicon">&#xe64a;</span>
     </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -48,6 +50,7 @@ export default {
         float right
         width 1.24rem
         text-align center
+        color #fff
         .arrowicon
             font-size .24rem
 
